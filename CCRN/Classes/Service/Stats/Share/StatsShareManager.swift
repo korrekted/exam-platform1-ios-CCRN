d500cb07a53998321346cc4c0848dbad6f54b539
Sergei Pankov <sergeipankov@Sergeis-MacBook-Air.local>
2022-06-02 19:15:27 +0100

    config

--- CCRN/Classes/Service/Stats/Share/StatsShareManager.swift
+++ CCRN/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.ccrn.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.ccrn.widget")
     }
     
     @discardableResult
