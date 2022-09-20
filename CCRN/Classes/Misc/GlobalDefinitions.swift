540f0e14f6330066b9c26c45fea8c969a77855e1
akonst17 <akonst17@gmail.com>
2022-09-20 12:15:44 +0300

    интеграция sdk 2.0; новые урлы; build 18

--- CCRN/Classes/Misc/GlobalDefinitions.swift
+++ CCRN/Classes/Misc/GlobalDefinitions.swift
@@ -9,18 +9,21 @@ final class GlobalDefinitions {
     static let domainUrl = "https://exams2.mastrapi.com"
     static let apiKey = "PoMZ7edCC7jgDK65"
 
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "PYCah6IK8ypcDIZT"
-
     static let applicationTag = "EXAM_CCRN"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "PYCah6IK8ypcDIZT"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "b1DxSpN9OeivnJ7j"
 
     static let appStoreUrl = "https://apps.apple.com/app/pass-ccrn/id1569413149"
     static let appleAppID = "1569413149"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
